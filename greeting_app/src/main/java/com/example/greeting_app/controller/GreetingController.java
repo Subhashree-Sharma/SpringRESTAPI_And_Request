1d66f860829b2e39b08bd2caf2df707346d22d83
Subhashree-Sharma <sharmasubhashree88@gmail.com>
2025-02-23 17:12:37 +0530

    [Subhashree Sharma]add.Extend GreetingController to use ServicesLayer

--- greeting_app/src/main/java/com/example/greeting_app/controller/GreetingController.java
+++ greeting_app/src/main/java/com/example/greeting_app/controller/GreetingController.java
@@ -1,5 +1,5 @@
 package com.example.greeting_app.controller;
-
+import com.example.greeting_app.service.GreetingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greeting")
 public class GreetingController {
 
-    // GET Request - Returns a simple greeting message
+    private final GreetingService greetingService;
+
+    //Constructor injection
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    //UC_01 (Commented out)
+    /*
     @GetMapping
     public String getGreeting() {
         return "Hello, Welcome to the Greeting App!";
+    }
+    */
+
+    //UC_02 - GET Request - Returns greeting from Service Layer
+    @GetMapping("/message")
+    public String getServiceGreeting() {
+        return greetingService.getGreetingMessage();
     }
 }
